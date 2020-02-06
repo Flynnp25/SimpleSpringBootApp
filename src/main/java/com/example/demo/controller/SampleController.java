0ea4e18b4b8e638ea1268d603ed28ee57b814237
Patrick Flynn <pflynn210392@gmail.com>
2020-02-06 23:06:44 +0000

    first commit

---
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/")
+    String getGreeting(){
+        return "Hello Paddy";
+    }
+
+}
